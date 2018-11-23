bc2fac63ea3e5ad4a134402d6adb3461fc82e7ea
Pedro Nascimento <lobo@estudar.com.vc>
2018-11-23 19:34:51 -0200

    Build: update build

--- dist/types/ValidationRunner.d.ts
+++ dist/types/ValidationRunner.d.ts
@@ -1,9 +1,11 @@
 import Validation from './Validation';
 import FormData from './FormData';
-export default class ValidationRunner<T> {
+export default class ValidationRunner<T extends {}> {
     protected validations: {
         [s: string]: Validation<T>;
     };
     constructor(...validations: Validation<T>[]);
     run(oldData: FormData<T>, name: string, value: any): FormData<T>;
+    runAll(data: T): FormData<T>;
+    static isValid<T extends {}>(data: FormData<T>): boolean;
 }
