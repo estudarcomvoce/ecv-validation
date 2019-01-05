cbc469198f751fb00259185b6935a9cb08e7063d
Pedro Nascimento <lobo@estudar.com.vc>
2019-01-05 14:58:05 -0200

    Fix: Export validationDataFactory on root

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export { default as ValidationData } from './ValidationData';
+export { default as ValidationData, ValidationDataFactory } from './ValidationData';
 export { default as Validation } from './Validation';
 export { default as ValidationRunner } from './ValidationRunner';
 export * from './validationRules';
